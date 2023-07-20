6ff2c600d8a6db075fdc6717b52d4a9122ebfaf5
RiccardoG1002 <riccardogallelli98@gmail.com>
2023-07-20 07:46:07 +0200

    created login view and added registration view's placeholder

--- lib/presentation/views/login_view.dart
+++ lib/presentation/views/login_view.dart
@@ -1,14 +1,89 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:login_flutter/presentation/views/registration_view.dart';
+import 'package:login_flutter/presentation/widgets/button_global.dart';
+import 'package:login_flutter/presentation/widgets/text_form_global.dart';
 import 'package:login_flutter/utils/colors_global.dart';
 
 class LoginView extends StatelessWidget {
-  const LoginView({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: GlobalColors.backGroundColor,
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Text(
+                'CULO',
+                style: TextStyle(
+                  color: GlobalColors.mainColor,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Login to your account',
+                style: TextStyle(
+                  color: GlobalColors.textColor,
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormGlobal(
+                controller: emailController,
+                text: 'Email',
+                textInputType: TextInputType.emailAddress,
+                obscured: false,
+              ),
+              const SizedBox(height: 10),
+              TextFormGlobal(
+                controller: passwordController,
+                text: 'Password',
+                textInputType: TextInputType.text,
+                obscured: true,
+              ),
+              const SizedBox(height: 10),
+              const ButtonGlobal(text: 'Login'),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Don\'t have an account? ',
+              style: TextStyle(
+                color: GlobalColors.textColor,
+              ),
+            ),
+            InkWell(
+              child: Text(
+                'Sign up!',
+                style: TextStyle(color: GlobalColors.accentColor),
+              ),
+              onTap: () {
+                Get.to(RegistrationView());
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
